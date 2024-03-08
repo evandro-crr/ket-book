540917b822e26e853e473cb54e416ede98af60bf
Evandro Chagas Ribeiro da Rosa <evandro@quantuloop.com>
2024-03-08 18:35:59 -0300

    update tutorial and add cname

--- publish.sh
+++ publish.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 rm -rf book/_build/html
+pipx run black\[jupyter\] book
 poetry run jupyter-book build book
-pipx run ghp-import -n -p -f book/_build/html
+pipx run ghp-import -n -p -f book/_build/html -c aprenda.quantumket.org
